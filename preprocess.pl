9df764768d9b969cd389f18a6c9924882a643065
Sridaran Thoniyil <sri7thon@gmail.com>
2023-05-30 00:29:55 -0400

    Add preprocessing for lalt + ralt functionality

--- preprocess.pl
+++ preprocess.pl
@@ -5,3 +5,8 @@ print $_;
 if (m/^exit_internal =/) {
     print q(exit = #(@exit_internal @kbd-set-backlight-off));
 }
+
+if (/lmet = exit/) {
+    print q(lalt = (tap-hold-next-release $tap-hold-delay-min @exit #(@exit_internal (layer-toggle leader))));
+    print q(ralt = (tap-hold-next-release $tap-hold-delay-min @exit #(@exit_internal (layer-toggle leader-no-block) 'ralt')));
+}
